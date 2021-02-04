bb2800bf6448aaade4d8a191f0b5ce668dccf914
dorridorsa <70901144+dorridorsa@users.noreply.github.com>
2021-02-04 16:31:35 +0100

    first commit

---
+++ precache-manifest.815e2bfbd041f5a9508c8bc706d2ecb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50d93972f7d224205f2",
+    "url": "/css/app.14bde8f9.css"
+  },
+  {
+    "revision": "6e59439a07cb23806d97",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "1a0586c05fb899335f2d420b571a7e2e",
+    "url": "/img/adam-solomon-WHUDOzd5IYU-unsplash.1a0586c0.jpg"
+  },
+  {
+    "revision": "d53b71e52366f8be2dd2e5760f682fb5",
+    "url": "/img/dorsa-logo.d53b71e5.png"
+  },
+  {
+    "revision": "56a738c3ee82d20dab7c4ba89097eda8",
+    "url": "/img/dorsa.png"
+  },
+  {
+    "revision": "9952baee1a4d6370f8c957afa195ff17",
+    "url": "/img/laura-chouette-yd3mg93Smn8-unsplash.9952baee.jpg"
+  },
+  {
+    "revision": "e6b5de8e170a2e8acaec98979d01feb6",
+    "url": "/img/scott-graham-5fNmWej4tAA-unsplash.e6b5de8e.jpg"
+  },
+  {
+    "revision": "f45b755b8562dd298a09fbd0bc010b62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b50d93972f7d224205f2",
+    "url": "/js/app.139fdc25.js"
+  },
+  {
+    "revision": "6e59439a07cb23806d97",
+    "url": "/js/chunk-vendors.07326f8a.js"
+  },
+  {
+    "revision": "3325de134a4c69228b57eefe9e98a42c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.815e2bfbd041f5a9508c8bc706d2ecb2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "dorsa-project"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute("https://makeup-api.herokuapp.com/api/v1/products.json", new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
